a919304cc036115dfdc8e46fddd148dc93b72b3d
Sean Murphy <sean@iamseanmurphy.com>
2025-02-12 04:21:05 +0000

    1.3.5

--- easy-accordion-block/build/blocks/accordion/index.asset.php
+++ easy-accordion-block/build/blocks/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cc91da85491eaf856d3d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '005a9525c62cd82a07ca');
